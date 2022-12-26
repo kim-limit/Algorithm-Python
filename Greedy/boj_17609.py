cf618895d07477267689e0ffec2f50c4db0461c7
kim-limit <rlawogks2468@naver.com>
2022-12-26 22:02:11 +0900

    화문

---
+++ Greedy/boj_17609.py
@@ -0,0 +1,67 @@
+# def checkIsSame(str1, str2): # 반나눴을때 같은지 확인하는 함수
+#     if str1 == str2[::-1]:
+#         return True
+
+# def checkIsContain(str1, str2):
+#     reversed = str2[::-1]
+#     if str1 == reversed[1:]:
+#         return True
+#     for i in range(1, len(reversed)):
+#         if str1 == reversed[0:i] + reversed[i+1:]:
+#             return True
+#     return False
+
+# for i in range(t):
+#     str = input()
+#     # 홀수일때
+#     if len(str) % 2 != 0: 
+#         if checkIsSame(str[:len(str)//2], str[len(str)//2+1:]):
+#             print(0) # 화문이면 0출력
+#         else: # 화문 아니면 반잘라서 확인
+#             if checkIsContain(str[:len(str)//2], str[len(str)//2:]):
+#                 print(1)
+#             elif checkIsContain(str[len(str)//2+1:], str[:len(str)//2+1]):
+#                 print(1)
+#             else:
+#                 print(2)
+#     else:
+#         if checkIsSame(str[:len(str)//2], str[len(str)//2:]):
+#             print(0) # 화문이면 0출력
+#         else:
+#             if checkIsSame(str[:len(str)//2 -1], str[len(str)//2+1:]):
+#                 print(1)
+#             else:
+#                 print(2)
+
+def finalCheck(str, left, right):
+    while left < right:
+        if str[left] == str[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
+
+def checkIsSame(str, left, right):
+    if str == str[::-1]: # 바로 화문인 경우
+        return 0
+    while left < right: # 1 or 2
+        if str[left] == str[right]:
+            left += 1
+            right -= 1
+        else:
+            leftCheck = finalCheck(str, left + 1, right)
+            rightCheck = finalCheck(str, left, right-1)
+            if leftCheck or rightCheck:
+                return 1
+            else:
+                return 2
+    return 1
+    
+t = int(input())
+
+for i in range(t):
+    str = input()
+    print(checkIsSame(str, 0, len(str)-1))
+
+    
