64cddf3de130fccf5848fe27fa5d1bad0d31b93d
kim-limit <rlawogks2468@naver.com>
2023-01-08 17:51:52 +0900

    이코테 효율적인 화폐구성

---
+++ DP/coin.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+d = [10001] * 10001
+
+coins = []
+for _ in range(n):
+    coin = int(input())
+    coins.append(coin)
+
+d[0] = 0
+for coin in coins:
+    for i in range(coin, m + 1):
+        if d[i - coin] != -1:
+            d[i] = min(d[i], d[i - coin] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
