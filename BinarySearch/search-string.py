649a101e15e0ed917e40ff61284f59a0ee0bb6ba
kim-limit <rlawogks2468@naver.com>
2023-01-08 17:52:13 +0900

    이코테 바닥 공사

--- BinarySearch/search-string.py
+++ BinarySearch/search-string.py
@@ -2,7 +2,7 @@ from bisect import bisect_left, bisect_right
 def count_by_range(array, left_value, right_value):
     left_index = bisect_left(array, left_value)
     right_index = bisect_right(array, right_value)
-    print(array, left_value, right_value)
+
     return right_index - left_index
 
 def solution(words, queries):
--- DP/ant.py
+++ DP/ant.py
@@ -3,7 +3,8 @@ data = list(map(int, input().split()))
 
 d = [0] * 1001
 d[0] = data[0]
-d[1] = data[1]
+d[1] = max(data[0], data[1])
 for i in range(2, n):
     d[i] = max(d[i - 1], d[i - 2] + data[i])
+    
 print(d[n - 1])
