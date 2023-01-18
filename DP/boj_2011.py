666ec8ab10b67ed543a50341ad35f581b76126e3
kim-limit <rlawogks2468@naver.com>
2023-01-19 00:40:30 +0900

    백준 암호코드

--- DP/boj_2011.py
+++ DP/boj_2011.py
@@ -1,33 +1,25 @@
-n = input()
+n = list(map(int,input()))
 
-dp = [0] * 5001
-result = 0
+lens = len(n)
+dp = [0] * (lens + 1)
 
-def fibo():
-    if n == '0':
-        return False
+# 맨 앞이 0이면 불가능
+if n[0] == 0:
+    print("0")
+else :
+    # dp를 위해 앞에 하나 추가
+    n = [0] + n
+    # 0번과 1번은 무조건 1
     dp[0] = 1
-    if len(n) == 1:
-        return True
-    if 1 <= int(n[:2]) <= 26 and n[1] != '0':
-        dp[1] = 2
-    else:
-        dp[1] = 1
-    if n[1] == '0' and 3 <= int(n[0]):
-        return False
-    for i in range(2, len(n)):
-        if n[i] == '0':
-            if n[i - 1] != '1' and n[i - 1] != '2':
-                return False
-            dp[i] = dp[i - 2]
-        else:
-            if 1 <= int(n[i - 1] + n[i]) <= 26:
-                dp[i] = dp[i - 1] + dp[i - 2]
-            else:
-                dp[i] = dp[i - 1]
-    return True
+    dp[1] = 1
 
-if fibo():
-    print(dp[len(n) - 1] % 1000000) 
-else:
-    print(0)
+    for i in range(2, lens + 1):
+        # 0보다 크면, 0이 아니면 1칸 전 그대로 감
+        if n[i] > 0:
+            dp[i] += dp[i - 1]
+        temp = n[i - 1] * 10 + n[i]
+        # 두개로 쪼갤 수 있으면 2칸 전도 더함
+        if temp >= 10 and temp <= 26 :
+            dp[i] += dp[i - 2]
+    # 결과 출력
+    print(dp[lens] % 1000000)
